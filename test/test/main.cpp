1c5f4af0ee0c9f83e7c68d80fe5e27ac9736ba40
boyang <boyang2021@gmail.com>
2020-02-21 23:18:12 -0600

    testing a new local repos

---
+++ test/test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Boyang Zhang on 2/21/20.
+//  Copyright © 2020 Boyang Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
